226692776f72a3c823280885c694248c85157aa2
DariaLopatina <151048961+DariaLopatina@users.noreply.github.com>
2024-06-02 01:28:43 +0300

    + object plot

--- main.py
+++ main.py
@@ -8,39 +8,84 @@ vector = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4]
 # m = np.array([[-1, 0], [0, -1]]) відзеркалення по х = у
 #m = np.array([[-1, 0], [0, 1]]) відзеркалення по у
 
+def plot(object, title):
+    plt.plot(object[:, 0], object[:, 1], marker='o')
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
+
 def rotate(object, angle):
     radians = np.radians(angle)
     rotate_matrix = np.array([[np.cos(radians), -np.sin(radians)],
-                                [np.sin(radians), np.cos(radians)]])
-    return object @ rotate_matrix.T
-
+                              [np.sin(radians), np.cos(radians)]])
+    title = f"rotation {angle}"
+    return object @ rotate_matrix.T, title
 
 def scale(object, factor):
     scale_matrix = np.array([[factor, 0], [0, factor]])
-    return object @ scale_matrix.T
-
+    title = f"scale {factor}"
+    return object @ scale_matrix.T, title
 
 def mirror(object, axis):
     if axis == 'x':
         mirror_matrix = np.array([[1, 0], [0, -1]])
     else:
         mirror_matrix = np.array([[-1, 0], [0, 1]])
-    return object @ mirror_matrix.T
 
-def title(object, axis, factor):
+    title = f"mirror {axis}"
+    return object @ mirror_matrix.T, title
+
+def tilt(object, axis, factor):
     if axis == 'x':
-        title_matrix = np.array([[1, factor], [0, 1]])
+        tilt_matrix = np.array([[1, factor], [0, 1]])
     else:
-        title_matrix = np.array([[1, 0], [factor, 1]])
-    return object @ title_matrix.T
+        tilt_matrix = np.array([[1, 0], [factor, 1]])
+
+    title = f"tilt {axis, factor}"
+    return object @ tilt_matrix.T, title
 
 def universal (object, matrix):
-    return object @ matrix
+    title = f"universal change {matrix}"
+    return object @ matrix, title
+
+def object_manipulation(object):
+    title = "vector"
+    plot(object, title)
+
+    result, title = rotate(object, 90)
+    plot(result, title)
+
+    result, title = scale(object, 2)
+    plot(result, title)
+
+    result, title = mirror(object, 'x')
+    plot(result, title)
+
+    result, title = tilt(object, 'x', 3)
+    plot(result, title)
+
+    matrix = np.array([[1, 2], [3, 4]])
+    result, title = universal(object, matrix)
+    plot(result, title)
 
 
 
-m = np.array([[-1, 0], [0, 1]])
-vector = vector @ m
-plt.plot(vector[:, 0], vector[:, 1])
 
-plt.show()
+
+object_manipulation(vector)
+
+
+
+
+#def ThreeD_manipulation():
+
+
+
+
+
+
+
+#m = np.array([[-1, 0], [0, 1]])
+#vector = vector @ m
+#plt.plot(vector[:, 0], vector[:, 1])
+#plt.show()
